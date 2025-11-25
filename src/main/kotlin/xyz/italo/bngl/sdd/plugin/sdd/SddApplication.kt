e6c513e7f08d16eb9cb84a1bb92110701188a0e4
Italo A. G. <italoag@users.noreply.github.com>
2025-11-25 10:52:10 -0300

    feat: refactoring to use koog and embabel

---
+++ src/main/kotlin/xyz/italo/bngl/sdd/plugin/sdd/SddApplication.kt
@@ -0,0 +1,11 @@
+package xyz.italo.bngl.sdd.plugin.sdd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SddApplication
+
+fun main(args: Array<String>) {
+	runApplication<SddApplication>(*args)
+}
---
+++ src/test/kotlin/xyz/italo/bngl/sdd/plugin/sdd/SddApplicationTests.kt
@@ -0,0 +1,15 @@
+package xyz.italo.bngl.sdd.plugin.sdd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+
+@Import(TestcontainersConfiguration::class)
+@SpringBootTest
+class SddApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
